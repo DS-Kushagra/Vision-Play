7d075a20105e8234eb987b0100743539e7923a7a
Kushagra <kushagraagrawal128@gmail.com>
2025-03-15 21:17:13 +0530

    Ball Control Tracking

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from utils import read_video, save_video
 from trackers import Tracker
 import cv2
 from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
+import numpy as np      
 
 def main():
     # Read video frames
@@ -22,11 +24,29 @@ def main():
     for frame_num, player_track in enumerate(tracks['players']):
         for player_id, player_track in player_track.items():
             team = team_assigner.get_player_team(video_frames[frame_num], player_track["bbox"], player_id)
+
             tracks["players"][frame_num][player_id]["team"] = team
             tracks["players"][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
+    
+
+    # Assign ball Aquisiton to player
+    player_assigner = PlayerBallAssigner()
+    team_ball_control = []
+
+    for frame_num, player_track in enumerate(tracks["players"]):
+        ball_bbox = tracks["ball"][frame_num][1]["bbox"]
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks["players"][frame_num][assigned_player]["has_ball"] = True
+            team_ball_control.append(tracks["players"][frame_num][assigned_player]["team"])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    
+    team_ball_control = np.array(team_ball_control)
 
     # Draw object tracks
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
 
     # Save video
     save_video(output_video_frames, 'Output-Videos/output_video.avi')
